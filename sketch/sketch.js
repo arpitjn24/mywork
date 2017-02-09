fa71c360d2ec009625f51797df861f2eb34eef5c
bigdaarpit <arpitjain0112@gmail.com>
2017-02-09 18:30:27 +0530

    all

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,41 +1,28 @@
-/**
- * Created by arpit on 8/2/17.
- */
-console.log(1);
-console.log(window.innerWidth);
-// for(j=0;j<10;j++)
-// {for(i=0;i<10;i++)
-//     document.getElementById('box').innerHTML+='<div  id="'+i+'"class="'+j+'" style="width: 5px;height: 25px;background-color: aqua"' +
-//         'onclick="color('+i+','+j+')">';
-//     document.getElementById('box').innerHTML+='<br><br><br><br><br><br><span></span>';
-//
-// }
-//
-// color=function (i,j) {
-//    // console.log(1);
-//     console.log(i,j);
-//     document.getElementById(''+i+'.'+j+'').style.backgroundColor="red";
-// }
-console.log(innerWidth)
-var flag=0;
-for(i=0;i<window.innerWidth;i++)
-{   document.getElementById('box').innerHTML+='<div  ' +
-    'id="'+i+'" style="width: 1px;height: 45px;background-color: aqua"' +
-       'onmousedown="col('+i+') " onmousemove="color('+i+') " onmouseup="cool('+i+')">';
-//  
-    
+ var flag=0;
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
 }
+var canvas = document.getElementById('box');
+var context = canvas.getContext('2d');
+ canvas.addEventListener('mousemove', function(evt) {
+     if (flag)
+     {       var mousePos = getMousePos(canvas, evt);
+     console.log(mousePos.x, mousePos.y);
+         context.rect(mousePos.x,mousePos.y,2,2)
+         context.fillStyle="blue";
+         context.fill();
+ }
+}, false);
 
 cool=function () {
     flag=0;
 }
-col=function (i) {
+col=function () {
     flag=1;
 
-}
-
-color=function (i) {
-    console.log(i);
-    if(flag)
-    document.getElementById(''+i+'').style.backgroundColor="red";
 }
